250800acceca2ff715ff0c3c3a9bad1756ca9ce9
Tia Eastwood <tia.eastwood@airelogic.com>
2022-03-17 19:52:31 +0000

    effects

--- js/main.js
+++ js/main.js
@@ -1,37 +1,55 @@
 // jquery
 // $(document).ready in jquery means the actions can only be run once the page has fully loaded - all actions go inside this
 
-// .show() changes css display style to inline
-
 $(document).ready(() => {
   const $catButton = $("#cat-button");
   const $catImage = $("#cat-image");
   const $puppyImage = $("#puppy-image");
   const $mysteryImage = $("#mystery-image");
-  const $grid2 = $("#grid-item-2");
-  const $fadeKitten = $("#fade-kitten");
+  const $showBtn = $("#show-button");
+  const $hideBtn = $("#hide-button");
   const $fadeOutBtn = $("#fadeOut-button");
   const $fadeInBtn = $("#fadeIn-button");
+  const $fadeToggleBtn = $("#fadeToggle-button");
+  const $slideUpBtn = $("#slideUp-button");
+  const $slideDownBtn = $("#slideDown-button");
+  const $slideToggleBtn = $("#slideToggle-button");
 
   $catButton.on("click", () => {
     $catImage.toggle();
   });
 
-  $grid2.on("mouseenter", () => {
+  $showBtn.on("click", () => {
     $puppyImage.show();
     $mysteryImage.hide();
   });
 
-  $grid2.on("mouseleave", () => {
+  $hideBtn.on("click", () => {
     $puppyImage.hide();
     $mysteryImage.show();
   });
 
   $fadeOutBtn.on("click", () => {
-    $fadeKitten.fadeOut(900);
+    $(".fade-img").fadeOut(900);
   });
 
   $fadeInBtn.on("click", () => {
-    $fadeKitten.fadeIn(900);
+    $(".fade-img").fadeIn(900);
+  });
+
+  $fadeToggleBtn.on("click", () => {
+    $(".fade-img").fadeToggle("slow");
+  });
+
+  $slideUpBtn.on("click", () => {
+    $(".slide-img").slideUp("slow");
+  });
+
+  $slideDownBtn.on("click", () => {
+    $(".slide-img").slideDown("slow");
+  });
+
+  $slideToggleBtn.on("click", () => {
+    $(".slide-img").slideToggle("slow");
   });
 });
